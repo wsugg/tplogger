3fb148644ba9dfdaf376b54cce1861ee79f8e802
wsugg <sugg.will@gmail.com>
2013-04-02 19:58:15 -0700

    rails new tplogger

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tplogger::Application.config.secret_token = '9586bebc357b5a5a8c726e11dba15611c215d5e69e6bf3bd685773401db5fd461daa9d7ed5cc0409651ae2f5b2c14ee7b93cf3506ecd7a19fcc8d95738beddc1'
